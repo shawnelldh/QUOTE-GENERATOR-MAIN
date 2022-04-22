c1e63fa9df617867fb47385995d315280c7dbd46
shawnelldh <shawnelldh@gmail.com>
2022-04-22 17:59:18 -0400

    2 files modified

---
+++ index.js
@@ -0,0 +1,33 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show Loading
+
+function loading() {
+   loader.hidden = false;
+   quoteContainer.hidden = true;
+
+}
+
+// Hide Loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+  }
+
+}
+
+// Get Quote from API
+async function getQuote() {
+    loading();
+    const proxyURL = 'https://whispering-tor-04671.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+
+
+}
